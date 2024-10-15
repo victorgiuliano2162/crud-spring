6eb3686c336c33f68b55d454a01e91e0f032d40e
victorgiuliano2162 <vitor.giuliano@gmail.com>
2024-10-15 16:55:16 -0300

    base client service

---
+++ back/src/main/java/crud/back/service/ClienteService.java
@@ -0,0 +1,39 @@
+package crud.back.service;
+
+import crud.back.dao.ClienteDAO;
+import crud.back.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteDAO clienteDAO;
+
+    public Cliente saveCliente(Cliente cliente) {
+        return clienteDAO.save(cliente);
+    }
+
+    public List<Cliente> getClientes() {
+        List<Cliente> clientes = new ArrayList<>();
+        clienteDAO.findAll().forEach(clientes::add);
+        return clientes;
+    }
+
+    public Cliente getClienteById(Long id) {
+        return clienteDAO.findById(id).orElseThrow();
+    }
+
+    public void deleteClienteById(Long id) {
+        clienteDAO.deleteById(id);
+    }
+
+    public Cliente updateCliente(Cliente cliente) {
+        clienteDAO.findById(cliente.getId()).orElseThrow();
+        return clienteDAO.save(cliente);
+    }
+}
